29400b430bc3603174719b288c839b66266ef332
Treasure Porth <Weasley@Rons-Mac-Pro.local>
2017-08-10 13:48:37 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const selenium = require("selenium-webdriver");
+const By = selenium.By;
+
+const driver = new selenium.Builder()
+    .forBrowser("chrome")
+    .build();
+
+driver.get(process.env.URL);
+
+const locators = {
+    inviteeForm: By.id("registrar"),
+    inviteeNameField: By.css("#registrar input[name='name']")
+}
+
+function addInvitee(name) {
+    driver.findElement(locators.inviteeNameField)
+        .sendKeys(name);
+    
+    driver.findElement(locators.inviteeForm).submit();
+}
+
+addInvitee("Muhamed Hassan");
+addInvitee("Steve Hunter");
